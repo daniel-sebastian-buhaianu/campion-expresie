335971b853ac6c7e1faf873a1c7e5c242192e3ff
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-25 04:55:48 +0200

    Update expresie.cpp - scor 100

--- expresie.cpp
+++ expresie.cpp
@@ -1,62 +1,58 @@
 #include <fstream>
-#define VALOARE_MAXIMA_N 1000
 using namespace std;
 int main()
 {
-	int N,
-	    v[VALOARE_MAXIMA_N],
-	    i,
-	    celMaiMareProdus = 0,
-	    alDoileaCelMaiMareProdus = 0,
-	    pozitiePrimulTermenCelMaiMareProdus = 0,
-	    pozitiePrimulTermenAlDoileaCelMaiMareProdus = 0;
 	ifstream citeste("expresie.in");
-	citeste >> N;
-	citeste >> v[0];
-	for (i = 1; i < N; i++)
+	int N; citeste >> N;
+	long long v[N], suma = 0;
+	for (int i = 0; i < N; i++)
 	{
 		citeste >> v[i];
-		int produs = v[i]*v[i-1];
-		if (produs > celMaiMareProdus)
-		{
-			alDoileaCelMaiMareProdus = celMaiMareProdus;
-			pozitiePrimulTermenAlDoileaCelMaiMareProdus
-				= pozitiePrimulTermenCelMaiMareProdus;
-			celMaiMareProdus = produs;
-			pozitiePrimulTermenCelMaiMareProdus = i-1;
-		}
-		else if (produs > alDoileaCelMaiMareProdus)
-		{
-			alDoileaCelMaiMareProdus = produs;
-			pozitiePrimulTermenAlDoileaCelMaiMareProdus = i-1;
-		}
+		suma += v[i];
 	}
 	citeste.close();
-	int suma = 0;
-	for (i = 0; i < N; i++)
+	long long sumaMaxima = suma;
+	for (int i = 2; i < N; i++)
 	{
-		if (i == pozitiePrimulTermenCelMaiMareProdus
-		    || i == pozitiePrimulTermenAlDoileaCelMaiMareProdus)
+		long long
+			sumaTriplet = v[i-2]
+					+ v[i-1]
+					+ v[i],
+			produsTriplet = v[i-2]
+					* v[i-1]
+					* v[i],
+			sumaPartiala = suma
+					- sumaTriplet
+					+ produsTriplet;
+		if (sumaPartiala > sumaMaxima)
 		{
-			if (i+1 == pozitiePrimulTermenCelMaiMareProdus
-			    || i+1 == pozitiePrimulTermenAlDoileaCelMaiMareProdus)
-			{
-				suma += v[i]*v[i+1]*v[i+2];
-				i += 2;
-			}
-			else
-			{
-				suma += v[i]*v[i+1];
-				i++;
-			}
+			sumaMaxima = sumaPartiala;
 		}
-		else
+	}
+	for (int i = 1; i < N-2; i++)
+	{
+		long long
+			sumaPereche1 = v[i] + v[i-1],
+			produsPereche1 = v[i] * v[i-1];
+		for (int j = i+2; j < N; j++)
 		{
-			suma += v[i];
+			long long
+				sumaPereche2 = v[j] + v[j-1],
+				produsPereche2 = v[j] * v[j-1],
+				sumaPartiala = suma
+						- sumaPereche1
+						- sumaPereche2
+						+ produsPereche1
+						+ produsPereche2;
+			if (sumaPartiala > sumaMaxima)
+			{
+				sumaMaxima = sumaPartiala;
+			}
 		}
 	}
 	ofstream scrie("expresie.out");
-	scrie << suma;
+	scrie << sumaMaxima;
 	scrie.close();
 	return 0;
 }
+
